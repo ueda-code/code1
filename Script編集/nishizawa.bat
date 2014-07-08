e079a5d6d4f18cecd1453853c3e0c7089d026e02
西澤 健司 <code.ueda@std.code.ac.jp>
2014-07-08 11:55:05 +0900

    テストメッセージ

---
+++ Script編集/nishizawa.bat
@@ -0,0 +1,11 @@
+net use *  /delete /yes
+
+net use f: \\SHIN-FSV\DATA_Root
+net use g: \\SHIN-NAS15\NAS15_root
+net use h: \\SHIN-NAS16\NAS16_root
+
+net use i: \\192.168.101.5\D_root shin5588n /user:Administrator
+net use j: \\192.168.101.5\E_root shin5588n /user:Administrator
+net use k: \\192.168.101.5\F_root shin5588n /user:Administrator
+net use l: \\192.168.101.5\G_root shin5588n /user:Administrator
+net use m: \\192.168.101.6\‰€•Û‘¶—p
